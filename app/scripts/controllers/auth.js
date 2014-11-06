e9a946c4134f07d58944823ac4569b1cf89f1896
Justin Park <jpark3000@gmail.com>
2014-11-06 16:24:27 -0600

    Extracts auth functionality into factory

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -7,34 +7,16 @@
  * # AuthCtrl
  * Controller of the todoApp
  */
-var baseUrl = "http://recruiting-api.nextcapital.com"
+
 
 angular.module('todoApp.controllers', [])
-  .controller('AuthCtrl', function ($scope, $http, $location, $window) {
-    $scope.signUp = function(data) {
-      $http.post(baseUrl + "/users", data).
-        success(function(data, status, headers, config) {
-          console.log(data);
-          $window.sessionStorage.user = data.api_token;
-          $window.sessionStorage.userId = data.id;
-          $location.path('/todos')
-        }).
-        error(function(data, status, headers, config) {
-          console.log(data);
-          console.log(status);
-        });
-    };
-
-    $scope.logIn = function(data) {
-      console.log(data)
-      $http.post(baseUrl + "/users/sign_in", data).
-        success(function(data, status, headers, config) {
-          console.log(data);
-          $location.path('/todos')
-        }).
-        error(function(data, status, headers, config) {
-          console.log(data);
-          console.log(status);
+  .controller('AuthCtrl', function ($scope, $location, auth) {
+    $scope.auth = function(data, url) {
+      auth.request(data, url)
+        .then(function() {
+          $location.path('/todos');
+        }, function() {
+          alert('error');
         });
     };
   });
