2dc2f8996d17a20100160b1bbcc363a04e4bf1da
Justin Park <jpark3000@gmail.com>
2014-11-06 16:25:19 -0600

    Implements signup/signin functionality
    utilizes promises
    stores user_id and api_token in sessionstorage

---
+++ app/scripts/services/auth.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('todoApp.services', [])
+  .factory('auth', function($http, $location, $window, $q) {
+    var service = {};
+    var baseUrl = "http://recruiting-api.nextcapital.com/users";
+
+    service.request = function(data, url) {
+      var deferred = $q.defer();
+      $http.post(baseUrl + url, data)
+        .success(function(data) {
+          $window.sessionStorage.userId = data.id;
+          $window.sessionStorage.user = data.api_token;
+          deferred.resolve(data)
+        })
+        .error(function(data) {
+          deferred.reject(data)
+        });
+      return deferred.promise;
+    }
+
+    return service;
+  })
