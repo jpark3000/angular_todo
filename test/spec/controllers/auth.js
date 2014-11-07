14f3e546173692baecc138848f12d24531b20ee7
Justin Park <jpark3000@gmail.com>
2014-11-06 22:35:02 -0600

    Specs successful login and redirect

---
+++ test/spec/controllers/auth.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('Controller: AuthCtrl', function () {
+
+
+  // load the controller's module
+  beforeEach(module('todoApp'));
+
+
+  var AuthCtrl,
+      scope,
+      location;
+      
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function($controller, $rootScope, $location) {
+
+    scope = $rootScope.$new();
+    location = $location;
+
+    AuthCtrl = $controller('AuthCtrl', {
+      $scope: scope,
+      $location: location
+    });
+  }));
+
+  it('redirects to the todos page on successful authentication', inject(function($q, auth) {
+    var deferred = $q.defer()
+    deferred.resolve()
+    spyOn(auth, 'request').and.returnValue(deferred.promise);
+    spyOn(location, 'path');
+
+    scope.authorize()
+    scope.$digest();
+
+    expect(location.path).toHaveBeenCalledWith('/todos');
+  }));
+});
