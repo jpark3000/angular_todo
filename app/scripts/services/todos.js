6c193923ab08dea16051fd47c995459031620cb8
Justin Park <jpark3000@gmail.com>
2014-11-07 19:16:35 -0600

    Implements service for Todos page
    Abstracts Todo GET, POST, and PUT into one function call
    Adds api_token before request is made

---
+++ app/scripts/services/todos.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('todoApp.services')
+  .factory('todos', function($http, $q, $window) {
+    var service = {};
+
+    service.request = function(method, url, data) {
+      var deferred = $q.defer();
+      $http[method](url, data)
+        .success(function(data) {
+          deferred.resolve(data);
+        })
+        .error(function(data) {
+          deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
+    service.sanitizeData = function(data) {
+      return {api_token: $window.sessionStorage.apiToken, todo: data}
+    };
+
+
+    service.requestUrl = function() {
+      return this.createUrl()
+              + '?api_token='
+              + $window.sessionStorage.apiToken
+    };
+
+    service.createUrl = function() {
+      return baseUrl 
+              + '/users/'
+              + $window.sessionStorage.userId
+              + '/todos'
+    }
+
+    return service;
+  });
