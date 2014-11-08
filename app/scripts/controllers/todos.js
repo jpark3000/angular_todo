01047c8e16ff947ae20bb95c98aa0f857f27c74d
Justin Park <jpark3000@gmail.com>
2014-11-07 19:17:58 -0600

    Implements TodosCtrl functionality for...
    Viewing all todos
    Creating a Todo
    Editing and saving a Todo
    Canceling editing of a todo

--- app/scripts/controllers/todos.js
+++ app/scripts/controllers/todos.js
@@ -1,6 +1,42 @@
 'use strict';
 
 angular.module('todoApp.controllers')
-  .controller('TodosCtrl', function ($scope) {
-    
+  .controller('TodosCtrl', function ($scope, todos) {
+
+    todos.request('get', todos.requestUrl())
+      .then(function(data) {
+        $scope.todos = data;
+      }, function() {
+        alert('error');
+      });
+
+    $scope.createTodo = function(data) {
+      var data = todos.sanitizeData(data)
+      todos.request('post', todos.createUrl(), data)
+        .then(function(data) {
+          $scope.todos.push(data);
+        }, function() {
+          alert('error');
+        });
+    };
+
+    $scope.edit = function(todo) {
+      $scope.editableTodo = angular.copy(todo)
+      todo.editable = true;
+    };
+
+    $scope.cancelEdit = function(todo) {
+      $scope.editableTodo = null;
+      todo.editable = false;
+    };
+
+    $scope.updateTodo = function(todo, index) {   
+      var data = todos.sanitizeData(todo) 
+      todos.request('put', todos.createUrl() + '/' + data.todo.id, data)
+        .then(function(data) {
+          $scope.todos[index] = data;
+        }, function(data) {
+          alert('error')
+        });
+    };
   });
