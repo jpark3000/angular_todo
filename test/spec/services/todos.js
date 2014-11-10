7ff4fcd781d5b0cbd4c9ced44a5c149ae7724768
Justin Park <jpark3000@gmail.com>
2014-11-09 18:54:25 -0600

    Adds tests for POST and PUT on todo resource

--- test/spec/services/todos.js
+++ test/spec/services/todos.js
@@ -18,8 +18,22 @@ describe('Services: todos', function() {
   });
 
   it('successfully retrieves a users todos', function() {
-    httpBackend.expectGET(baseUrl + '/users/1/todos?api_token=1234').respond(200, '')
-    todos.request('get', baseUrl + '/users/1/todos?api_token=1234').then(function(data) { console.log(data)})
+    httpBackend.expectGET(baseUrl + '/users/1/todos?api_token=1234').respond(200);
+    todos.request('get', baseUrl + '/users/1/todos?api_token=1234');
+    httpBackend.flush();
+  });
+
+  it('successfully edits a users todos', function() {
+    var putData = {api_token: '1234', todo: {description: 'Get Milk', is_complete: true}};
+    httpBackend.expectPUT(baseUrl + '/users/1/todos/1', putData).respond(200);
+    todos.request('put', baseUrl + '/users/1/todos/1', putData);
+    httpBackend.flush();
+  });
+
+  it('successfully creates a users todo', function() {
+    var postData = {api_token: '1234', todo: {description: 'Get Eggs'}};
+    httpBackend.expectPOST(baseUrl + '/users/1/todos', postData).respond(201);
+    todos.request('post', baseUrl + '/users/1/todos', postData)
     httpBackend.flush()
   });
 });
